1c1bb3d56762310f73678d13173c9ed8ca33d1a0
pfefferle <pfefferle@gmail.com>
2014-05-08 21:13:57 +0200

    nicer readme and optimized packaging configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,96 +1,96 @@
 module.exports = function (grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			expanded: {
-				options: {
-					style: 'expanded'
-				},
-				files: {
-					'css/openwebicons.css': 'sass/openwebicons.scss',
-					'css/openwebicons-bootstrap.css': 'sass/openwebicons-bootstrap.scss',
-					'css/openwebicons-cdn.css': 'sass/openwebicons-cdn.scss'
-				}
-			},
-			compressed: {
-				options: {
-					style: 'compressed'
-				},
-				files: {
-					'css/openwebicons.min.css': 'sass/openwebicons.scss',
-					'css/openwebicons-bootstrap.min.css': 'sass/openwebicons-bootstrap.scss',
-					'css/openwebicons-cdn.min.css': 'sass/openwebicons-cdn.scss'
-				}
-			},
-			compact: {
-				options: {
-					style: 'compact'
-				},
-				files: {
-					'css/weloveiconfonts.css': 'sass/weloveiconfonts.scss'
-				}
-			}
-		},
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      expanded: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'css/openwebicons.css': 'sass/openwebicons.scss',
+          'css/openwebicons-bootstrap.css': 'sass/openwebicons-bootstrap.scss',
+          'css/openwebicons-cdn.css': 'sass/openwebicons-cdn.scss'
+        }
+      },
+      compressed: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/openwebicons.min.css': 'sass/openwebicons.scss',
+          'css/openwebicons-bootstrap.min.css': 'sass/openwebicons-bootstrap.scss',
+          'css/openwebicons-cdn.min.css': 'sass/openwebicons-cdn.scss'
+        }
+      },
+      compact: {
+        options: {
+          style: 'compact'
+        },
+        files: {
+          'css/weloveiconfonts.css': 'sass/weloveiconfonts.scss'
+        }
+      }
+    },
 
-		update_json: {
-			// update bower.json with data from package.json
-			bower: {
-				src: 'package.json', // where to read from
-				dest: 'bower.json', // where to write to
-				// the fields to update, as a String Grouping
-				fields: {
-					'name': null,
-					'version': null,
-					'description': null
-				}
-			},
-			// update component.json with data from package.json
-			// component.json fields are a named a bit differently from
-			// package.json, so let's tell update_json how to map names
-			component: {
-				src: 'package.json',
-				// reuse the task-level `src`
-				dest: 'component.json', // where to write to
-				fields: { // the fields to update
-					// notice how this time we're passing a hash instead
-					// of an array; this allows us to map the field names.
-					// We still specify all the names we want, and additionally
-					// we also specify the target name in the detination file.
-					// to            from
-					// -----------   -------------------
-					'name': null, // null means 'leave as is'
-					'description': 'description', // but feel free to type the field name twice
-					'version': null,
-					'keywords': null,
-					'main': null,
-					'development': 'devDependencies',
-					'license': null
-				}
-			},
-			// `composer` has the same data as `package`, but has some tricky
-			// semantics
-			composer: {
-				src: 'package.json',
-				// again, reuse the task-level `src`
-				dest: 'composer.json',
-				// the fields in an Array Grouping with some embedded Object Groupings
-				fields: {
-					'name': null,
-					'description': null,
-					'keywords': null,
-					'license': null
-				}
-			}
-		}
-	});
+    update_json: {
+      // update bower.json with data from package.json
+      bower: {
+        src: 'package.json', // where to read from
+        dest: 'bower.json', // where to write to
+        // the fields to update, as a String Grouping
+        fields: {
+          'name': null,
+          'version': null,
+          'description': null
+        }
+      },
+      // update component.json with data from package.json
+      // component.json fields are a named a bit differently from
+      // package.json, so let's tell update_json how to map names
+      component: {
+        src: 'package.json',
+        // reuse the task-level `src`
+        dest: 'component.json', // where to write to
+        fields: { // the fields to update
+          // notice how this time we're passing a hash instead
+          // of an array; this allows us to map the field names.
+          // We still specify all the names we want, and additionally
+          // we also specify the target name in the detination file.
+          // to            from
+          // -----------   -------------------
+          'name': null, // null means 'leave as is'
+          'description': 'description', // but feel free to type the field name twice
+          'version': null,
+          'keywords': null,
+          'main': null,
+          'development': 'devDependencies',
+          'license': null
+        }
+      },
+      // `composer` has the same data as `package`, but has some tricky
+      // semantics
+      composer: {
+        src: 'package.json',
+        // again, reuse the task-level `src`
+        dest: 'composer.json',
+        // the fields in an Array Grouping with some embedded Object Groupings
+        fields: {
+          'name': null,
+          'description': null,
+          'keywords': null,
+          'license': null
+        }
+      }
+    }
+  });
 
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-update-json');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-update-json');
 
-	// Default task(s).
-	grunt.registerTask('default', ['sass', 'update_json']);
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'update_json']);
 
 };
--- README.md
+++ README.md
@@ -1,8 +1,56 @@
-OpenWeb Icons
-============
+# OpenWeb Icons
 
-The *OpenWeb Icons* are a set of icons to show the support for the *Open Web*!
+The *OpenWeb Icons* is a web-font that gives you scalable vector icons/logos of some open community, standards or projects.
+
+![OpenWeb Icons](http://pfefferle.github.io/openwebicons/img/OpenWeb-Icons.jpg)
+
+It includes the Creative Commons-, HTML5- and Microformats-icons for example. Use them to show your love for the *Open Web*!
+
+## Installation
+
+bower ([bower.io](http://bower.io/search/?q=openwebicons))
+
+    $ bower install openwebicons
+
+component
+
+    $ component install pfefferle/openwebicons
+
+npm ([npmjs.org](https://www.npmjs.org/package/openwebicons))
+
+    $ npm install openwebicons
+
+## Project Page
 
 You can find more informations and a list of all icons here: http://pfefferle.github.io/openwebicons/
+
+## Changelog
+
+* **2014-06-02**: Added WebMention, JavaScript (Community) and WTFPL icon
+* **2013-12-16**: Added Copyleft und GNU icons
+* **2013-10-29**: Added SIL OFL icons
+* **2013-08-22**: And added Tent icon
+* **2013-08-14**: Optimized Webfinger icon and added JSON-LD and SVG icon
+* **2013-08-06**: Added PubSubHubbub icon
+* **2013-07-16**: Added Bitcoin icon
+* **2013-06-29**: Added Webfinger icon
+* **2013-03-20**: Added *Open Source Initiative* and RDF icons
+* **2013-03-19**: More HTML5 icons, the git icon and the webhooks icon
+* **2013-02-17**: IndieWebIcons icons
+* **2013-01-30**: Microformats icons
+* **2012-10-03**: Added markdown and ToS;DR icon
+* **2012-09-29**: Added OData icon and made a nicer DataPortability icon
+* **2012-09-28**: Added Persona icon, resized "simple" icons and fixed some page issues
+* **2012-06-09**: Changed the FOAF and QR-Code icon
+* **2012-06-09**: Added Information-Card and BrowserID icon
+* **2012-06-04**: Added ePub, QR-Code, FOAF and OpenGraph icon
+* **2012-05-30**: Added semantics and connectivity icon
+* **2012-05-29**: Added XMPP, HTML5 and CSS3 icon
+* **2012-05-29**: Changed from letters to the private unicode range
+* **2012-05-23**: Added *Web Intents* Icon
+* **2012-05-05**: Added *federated social web*, *DataPortability* and *Open Web Foundation* Icon
+* **2012-05-03**: Initial Release
+
+---
 
 [![endorse](http://api.coderwall.com/pfefferle/endorsecount.png)](http://coderwall.com/pfefferle)
